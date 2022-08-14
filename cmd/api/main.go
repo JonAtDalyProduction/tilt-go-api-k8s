1602f1dc425471a827975c9c5a5a3de1760baedf
jonatdalyproduction <jsdaly@dalyproduction.com>
2022-08-13 21:01:23 -0700

    using chi router and cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,25 +4,32 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"net/http"
 	"os"
-
-	envloader "github.com/JonAtDalyProduction/go-env-loader"
 )
 
 type AppConfig struct {
 	HttpPort string `env:"HTTP_PORT,required=false"`
 }
 
-var appConfig AppConfig
+var config AppConfig
+
+type App struct {
+	config *AppConfig
+}
+
+var app App
 
 func init() {
-	envloader.ParseEnv(&appConfig, "dev.env")
-	//appConfig.HttpPort = "3333"
+	//envloader.ParseEnv(&config, "dev.env")
+	config.HttpPort = "3333"
+
 }
-func getRoot(w http.ResponseWriter, r *http.Request) {
+func (a *App) getRoot(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	(w).Header().Set("Access-Control-Allow-Origin", "*")
 	res := make(map[string]string)
 	res["message"] = "Go API is Working"
 	jsonRes, err := json.Marshal(res)
@@ -35,11 +42,44 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (a *App) postRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	res := make(map[string]string)
+	res["message"] = "Got post message from frontend"
+	jsonRes, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+	_, err = w.Write(jsonRes)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", getRoot)
-	fmt.Printf("starting api server on port %s", appConfig.HttpPort)
-	err := http.ListenAndServe(fmt.Sprintf(":%s", appConfig.HttpPort), mux)
+	app.config = &config
+	r := chi.NewRouter()
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://localhost", "http://api.localhost", "http://localhost:5173", "http://localhost:3333"},
+		//AllowOriginFunc:    nil,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+		//OptionsPassthrough: false,
+		//	Debug:              false,
+	}))
+	//Base route
+	r.Get("/", app.getRoot)
+	r.Post("/", app.postRoot)
+	fmt.Printf("starting api server on port %s\n", config.HttpPort)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", config.HttpPort), r)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/jonatdalyproduction/tilt-go-api-k8s
 
 go 1.18
 
-require github.com/JonAtDalyProduction/go-env-loader v1.1.1 // indirect
+require (
+	github.com/JonAtDalyProduction/go-env-loader v1.1.1 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
